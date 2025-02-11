4dcb2594fbfb3b0abc243f7251ec2bfaeb3a043c
Oz Tamir <oz@blockaid.co>
2025-02-11 23:55:50 +0200

    feat: Improve blog post card

--- src/components/BlogPosts.tsx
+++ src/components/BlogPosts.tsx
@@ -12,35 +12,35 @@ const BlogPosts = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 pb-10 border-b-2 border-b-golden/20 lg:mb-10 mb-4">
-      <h2 className="text-2xl font-bold mb-4 text-center dark:text-stone-200">
+    <div className="container mx-auto px-4 pb-6 border-b-2 border-b-golden/20 lg:mb-6 mb-4">
+      <h2 className="text-xl font-bold mb-4 text-center dark:text-stone-200">
         <a href="https://posts.oztamir.com">
           <code>Latest Blog Posts</code>
         </a>
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
           <div
             key={post.id}
-            className="max-w-sm rounded-2xl overflow-hidden shadow-lg cursor-pointer hover:shadow-xl transition-shadow duration-300 dark:bg-zinc-600
-                        dark:border-2 dark:border-stone-200/50"
+            className="rounded-lg overflow-hidden shadow-md cursor-pointer hover:shadow-lg transition-shadow duration-300 dark:bg-zinc-600
+                        dark:border dark:border-stone-200/50"
             onClick={() => window.open(`${post.url}`, "_blank")}
           >
             <img
-              className="w-full h-28 object-cover border-b-2 border-b-golden/20 dark:border-stone-200/50"
+              className="w-full h-20 object-cover border-b border-b-golden/20 dark:border-stone-200/50"
               src={post.feature_image}
               alt={post.title}
             />
-            <div className="px-6 py-4">
-              <div className="font-bold text-md mb-2 dark:text-stone-200">
+            <div className="px-4 py-3">
+              <div className="font-bold text-sm mb-1 dark:text-stone-200">
                 {post.title}
               </div>
-              <p className="text-gray-700 dark:text-stone-300 text-base">
-                {post.title.length < 50
+              <p className="text-gray-700 dark:text-stone-300 text-xs">
+                {post.title.length < 60
                   ? post.excerpt
                   : post.excerpt.slice(
                       0,
-                      post.excerpt.slice(0, 50).lastIndexOf(" ")
+                      post.excerpt.slice(0, 60).lastIndexOf(" ")
                     ) + "..."}
               </p>
             </div>
